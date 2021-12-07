f90ae13c39a7cee42bbbfe88e66da144b7fa6753
anoju <zun5761@gmail.com>
2021-12-07 23:11:25 +0900

    button. 컴포넌트 추가

---
+++ src/components/global/uiButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    ref="button"
+    type="button"
+    class="button"
+    :disabled="disabled"
+    v-on="listeners"
+  >
+    <strong v-if="$slots.prev">
+      <slot name="prev" />
+    </strong>
+    <span v-if="$slots.default">
+      <slot />
+    </span>
+    <strong v-if="$slots.next">
+      <slot name="next" />
+    </strong>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'UiButton',
+  props: {
+    disabled: { type: Boolean, default: false },
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    listeners() {
+      if (this.disabled) {
+        return null
+      }
+      return this.$listeners
+    },
+  },
+  methods: {
+  },
+}
+</script>
