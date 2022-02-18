277dd48f2ba075c76086594162aa066d04a57ecd
zun5761 <zun5761@gmail.com>
2022-02-18 14:20:41 +0900

    복사하기 기능

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -4,6 +4,20 @@
     <h1 class="t-center">
       index
     </h1>
+    <div class="copy--box">
+      {{ code }}
+      <ui-button
+        class="line small"
+        @click="copyTestingCode"
+      >
+        복사하기
+      </ui-button>
+      <input
+        ref="copyInput"
+        type="hidden"
+        :value="code"
+      >
+    </div>
     <div
       class="gd_index-box"
     >
@@ -28,9 +42,27 @@ export default {
   },
   data() {
     return {
+      code: '1234-qwer-5678-asdf',
     }
   },
   methods: {
+    copyTestingCode() {
+      const testingCodeToCopy = this.$refs.copyInput
+      testingCodeToCopy.setAttribute('type', 'text')
+      testingCodeToCopy.select()
+
+      try {
+        const successful = document.execCommand('copy')
+        const msg = successful ? '완료' : '실패'
+        alert(`테스트 코드 복사 ${msg}`)
+      } catch (err) {
+        alert('웁스, 복사 안되지롱')
+      }
+
+      /* unselect the range */
+      testingCodeToCopy.setAttribute('type', 'hidden')
+      window.getSelection().removeAllRanges()
+    },
   },
 }
 </script>
@@ -43,5 +75,22 @@ export default {
   justify-content: center;
   padding: 20px;
   font-size: 1.6rem;
+}
+</style>
+<style lang="scss" scoped>
+.copy--box{
+  position: relative;
+  text-align: center;
+  padding: 20px;
+  font-size: 1.8rem;
+  input{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    // width: 0;
+    // height: 0;
+    opacity: 0;
+    // z-index: -1;
+  }
 }
 </style>
